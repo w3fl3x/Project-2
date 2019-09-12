62e56b85fdc9945f4727fcc8d1c7590e29e4193e
winTS <huynhts@gmail.com>
2019-09-11 18:46:19 -0700

    lint fix errors for api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -21,18 +21,21 @@ module.exports = function(app) {
     });
   });
 
-    // Update an example
-    app.post("/api/examples", function(req, res) {
-      db.Movies.update({
+  // Update an example
+  app.post("/api/examples", function(req, res) {
+    db.Movies.update(
+      {
         seen: true
-      }, { 
+      },
+      {
         where: {
           id: req.body.id
         }
-      }).then(function(dbMovies) {
-        res.json(dbMovies);
-      });
+      }
+    ).then(function(dbMovies) {
+      res.json(dbMovies);
     });
+  });
 
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {
