7357c469dd5ce2a09ab5b1e992f9fef0ed1653c7
winTS <huynhts@gmail.com>
2019-09-11 18:28:08 -0700

    modified update function for api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -21,6 +21,19 @@ module.exports = function(app) {
     });
   });
 
+    // Update an example
+    app.post("/api/examples", function(req, res) {
+      db.Movies.update({
+        seen: true
+      }, { 
+        where: {
+          id: req.body.id
+        }
+      }).then(function(dbMovies) {
+        res.json(dbMovies);
+      });
+    });
+
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {
     db.Movies.destroy({ where: { id: req.params.id } }).then(function(
